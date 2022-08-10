69f0b239e362a1f3513881cbc406f4ba21cce693
rx3s-do <son.do@tpssoft.com>
2022-08-10 10:25:10 +0700

    add sample cloud config

---
+++ src/test/java/com/cloud/config/SampleConfig/SampleConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.config.SampleConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
